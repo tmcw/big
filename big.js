991c3f195397c7fc24867a05c51cdcfcdabc8de1
Tom MacWright <tom@macwright.org>
2011-10-20 12:23:55 -0400

    Golf it to 1k

--- big.js
+++ big.js
@@ -1,14 +1,9 @@
 window.onload = function() {
     var s = document.getElementsByTagName('div'), cur = 0;
     if (!s) return;
-
-    function hide() {
-        for (var k = 0; k < s.length; k++) s[k].style.display = 'none';
-    }
-
     function go(n) {
-        hide();
-        var i = 1000;
+        var i = 1e3;
+        for (var k = 0; k < s.length; k++) s[k].style.display = 'none';
         s[n].style.display = 'inline';
         s[n].style.fontSize = i + 'px';
         while (
@@ -19,21 +14,14 @@ window.onload = function() {
         }
         window.location.hash = n;
     }
-
     document.onclick = function() {
         cur = ++cur % (s.length);
         go(cur);
     };
-
     document.onkeydown = function(e) {
-        if (e.which === 39) {
-            go(cur = Math.min(s.length - 1, ++cur));
-        }
-        if (e.which === 37) {
-            go(cur = Math.max(0, --cur));
-        }
+        (e.which === 39) && go(cur = Math.min(s.length - 1, ++cur));
+        (e.which === 37) && go(cur = Math.max(0, --cur));
     };
-
     if (window.location.hash) cur = Math.max(
         Math.min(
             s.length - 1,
