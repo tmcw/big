6cf1872b933998ac03731dca12fc99fcc853f07d
Brian Campbell <lambda@continuation.org>
2011-10-30 22:56:03 -0400

    Don't repeat s[n]

--- big.js
+++ big.js
@@ -2,20 +2,20 @@ window.onload = function() {
     var s = document.getElementsByTagName('div'), cur = 0;
     if (!s) return;
     function go(n) {
-        var i = 1e3;
+        var i = 1e3, e = s[n];
         for (var k = 0; k < s.length; k++) s[k].style.display = 'none';
-        s[n].style.display = 'inline';
-        s[n].style.fontSize = i + 'px';
-        if (s[n].firstChild.nodeName === 'IMG') {
-            document.body.style.backgroundImage = 'url(' + s[n].firstChild.src + ')';
-            s[n].firstChild.style.display = 'none';
+        e.style.display = 'inline';
+        e.style.fontSize = i + 'px';
+        if (e.firstChild.nodeName === 'IMG') {
+            document.body.style.backgroundImage = 'url(' + e.firstChild.src + ')';
+            e.firstChild.style.display = 'none';
         } else {
             document.body.style.backgroundImage = '';
         }
         while (
-            s[n].offsetWidth > window.innerWidth ||
-            s[n].offsetHeight > window.innerHeight) {
-            s[n].style.fontSize = (i -= 10) + 'px';
+            e.offsetWidth > window.innerWidth ||
+            e.offsetHeight > window.innerHeight) {
+            e.style.fontSize = (i -= 10) + 'px';
         }
         if (window.location.hash !== n) window.location.hash = n;
     }
