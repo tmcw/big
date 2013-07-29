f5ae881ef6cff4a4277c0b45abefef6259d3dfd5
Tom MacWright <tom@macwright.org>
2013-07-29 15:45:17 -0400

    Style tweaks, support IE7

--- big.js
+++ big.js
@@ -10,7 +10,7 @@ window.onload = function() {
         if (e.firstChild.nodeName === 'IMG') {
             document.body.style.backgroundImage = 'url(' + e.firstChild.src + ')';
             e.firstChild.style.display = 'none';
-            e.classList.add('imageText');
+            if ('classList' in e) e.classList.add('imageText');
         } else {
             document.body.style.backgroundImage = '';
             document.body.style.backgroundColor = e.style.backgroundColor;
