0dd270f7d56ae8329aa7f4763cb4231c4d41a5a8
andy matthews <andy@commadelimited.com>
2014-05-09 14:08:06 -0500

    Add per-slide body classes from data attribute

--- big.js
+++ big.js
@@ -4,6 +4,12 @@ window.onload = function() {
     function go(n) {
         cur = n;
         var i = 1e3, e = s[n], t;
+        // if the div has a bodyclass data attribute, add it to the body
+        if (e.dataset['bodyclass'] !== undefined) {
+            document.body.className = e.dataset['bodyclass'];
+        } else {
+            document.body.className = '';
+        }
         for (var k = 0; k < s.length; k++) s[k].style.display = 'none';
         e.style.display = 'inline';
         e.style.fontSize = i + 'px';
