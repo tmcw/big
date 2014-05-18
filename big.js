8ded2c054191ab7b3cc55facba9756be05e9ac68
andy matthews <andy@commadelimited.com>
2014-05-17 20:29:33 -0500

    Only expose fwd, rev, and cur to 'big' object

--- big.js
+++ big.js
@@ -1,13 +1,14 @@
 window.onload = function() {
     big = {};
-    big.s = document.getElementsByTagName('div'), big.cur = 0, big.ti;
-    if (!big.s) return;
-    big.go = function(n) {
+    big.cur = 0;
+    var s = document.getElementsByTagName('div'), ti;
+    if (!s) return;
+    go = function(n) {
         big.cur = n;
-        var i = 1e3, e = big.s[n], t;
+        var i = 1e3, e = s[n], t;
         // if the div has a bodyclass data attribute, add it to the body
         document.body.className = e.dataset.bodyclass || '';
-        for (var k = 0; k < big.s.length; k++) big.s[k].style.display = 'none';
+        for (var k = 0; k < s.length; k++) s[k].style.display = 'none';
         e.style.display = 'inline';
         e.style.fontSize = i + 'px';
         if (e.firstChild.nodeName === 'IMG') {
@@ -18,9 +19,9 @@ window.onload = function() {
             document.body.style.backgroundImage = '';
             document.body.style.backgroundColor = e.style.backgroundColor;
         }
-        if (big.ti !== undefined) window.clearInterval(big.ti);
+        if (ti !== undefined) window.clearInterval(ti);
         t = parseInt(e.dataset.timeToNext || 0, 10);
-        if (t > 0) big.ti = window.setTimeout(big.fwd, (t * 1000));
+        if (t > 0) ti = window.setTimeout(big.fwd, (t * 1000));
         while (
             e.offsetWidth > window.innerWidth ||
             e.offsetHeight > window.innerHeight) {
@@ -31,9 +32,9 @@ window.onload = function() {
         if (window.location.hash !== n) window.location.hash = n;
         document.title = e.textContent || e.innerText;
     };
-    document.onclick = function() { big.go(++cur % (big.s.length)); };
-    big.fwd = function() { big.go(Math.min(big.s.length - 1, ++big.cur)); };
-    big.rev = function() { big.go(Math.max(0, --big.cur)); };
+    document.onclick = function() { go(++big.cur % (s.length)); };
+    big.fwd = function() { go(Math.min(s.length - 1, ++big.cur)); };
+    big.rev = function() { go(Math.max(0, --big.cur)); };
     document.onkeydown = function(e) {
         if (e.which === 39) big.fwd();
         if (e.which === 37) big.rev();
@@ -48,13 +49,13 @@ window.onload = function() {
     };
     big.parse_hash = function() {
         return Math.max(Math.min(
-            big.s.length - 1,
+            s.length - 1,
             parseInt(window.location.hash.substring(1), 10)), 0);
     };
     if (window.location.hash) big.cur = big.parse_hash() || big.cur;
     window.onhashchange = function() {
         var c = big.parse_hash();
-        if (c !== big.cur) big.go(c);
+        if (c !== big.cur) go(c);
     };
-    big.go(big.cur);
+    go(big.cur);
 };
