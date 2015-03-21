e1608937764e81f0aaf4631f808694c48898eb1a
Stuart P. Bentley <stuart@testtrack4.com>
2015-03-21 00:57:21 +0000

    Handle resize event
    This also optimizes the text sizing by starting from the inner height
    of the window rather than 1000. For windows shorter than 1000px,
    this saves hundreds of unnecessary layouts; for windows taller
    than 1000px, this lets text be truly as big as possible.

--- big.js
+++ big.js
@@ -2,13 +2,20 @@ window.onload = function() {
     var s = document.getElementsByTagName('div'), ti;
     if (!s) return;
     big = { current: 0, forward: fwd, reverse: rev, go: go, length: s.length };
+    function resize() {
+        var w = window.innerWidth, h = window.innerHeight, e = s[big.current];
+        e.style.fontSize = h + 'px';
+        for (var i = h-2; e.offsetWidth > w || e.offsetHeight > h; i-=2) {
+            e.style.fontSize = i + 'px';
+        }
+        e.style.marginTop = ((h - e.offsetHeight) / 2) + 'px';
+    }
     function go(n) {
         big.current = n;
-        var i = 1e3, e = s[n], t = parseInt(e.dataset.timeToNext || 0, 10);
+        var e = s[n], t = parseInt(e.dataset.timeToNext || 0, 10);
         document.body.className = e.dataset.bodyclass || '';
         for (var k = 0; k < s.length; k++) s[k].style.display = 'none';
         e.style.display = 'inline';
-        e.style.fontSize = i + 'px';
         if (e.firstChild && e.firstChild.nodeName === 'IMG') {
             document.body.style.backgroundImage = 'url("' + e.firstChild.src + '")';
             e.firstChild.style.display = 'none';
@@ -19,11 +26,7 @@ window.onload = function() {
         }
         if (ti !== undefined) window.clearInterval(ti);
         if (t > 0) ti = window.setTimeout(fwd, (t * 1000));
-        while ((e.offsetWidth > window.innerWidth ||
-            e.offsetHeight > window.innerHeight) && i >= 0) {
-            e.style.fontSize = (i -= 2) + 'px';
-        }
-        e.style.marginTop = ((window.innerHeight - e.offsetHeight) / 2) + 'px';
+        resize();
         if (window.location.hash !== n) window.location.hash = n;
         document.title = e.textContent || e.innerText;
     }
@@ -51,5 +54,6 @@ window.onload = function() {
         var c = parse_hash();
         if (c !== big.current) go(c);
     };
+    window.onresize = resize;
     go(big.current);
 };
