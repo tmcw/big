127d6e139e76f303a73da7fb3c7d60928d7a9c80
Tom Lee <thomas.j.lee@gmail.com>
2017-01-22 13:33:31 -0600

    use unicode icons instead of fontawesome SVGs

--- big.js
+++ big.js
@@ -17,9 +17,9 @@ window.onload = function() {
   for(var ai = 0; ai < audioCandidates.length; ai++)
     if (audioCandidates[ai].textTracks.length === 1 && audioCandidates[ai].textTracks[0].cues.length > 0) {
       big.audio = audioCandidates[ai];
-      var playControl = document.createElement('img');
-      playControl.style = 'width:30px;height:30px;';
+      var playControl = document.createElement('div');
       playControl.id = 'audio-control';
+      playControl.style = 'padding:5px';
       playControl.onclick = function(e) {
         if (big.audio.paused) {
           if (big.current === 0)
@@ -34,7 +34,7 @@ window.onload = function() {
       document.getElementsByTagName('body')[0].appendChild(playControl);
       window.setInterval(function() {
         if (!big.audio.paused) {
-          document.getElementById('audio-control').src = 'data:image/svg+xml;charset=utf-8,%3Csvg%20width%3D%221792%22%20height%3D%221792%22%20viewBox%3D%220%200%201792%201792%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cpath%20d%3D%22M832%201184v-576q0-14-9-23t-23-9h-256q-14%200-23%209t-9%2023v576q0%2014%209%2023t23%209h256q14%200%2023-9t9-23zm448%200v-576q0-14-9-23t-23-9h-256q-14%200-23%209t-9%2023v576q0%2014%209%2023t23%209h256q14%200%2023-9t9-23zm384-288q0%20209-103%20385.5t-279.5%20279.5-385.5%20103-385.5-103-279.5-279.5-103-385.5%20103-385.5%20279.5-279.5%20385.5-103%20385.5%20103%20279.5%20279.5%20103%20385.5z%22%20fill%3D%22%23aaa%22%2F%3E%3C%2Fsvg%3E';
+          document.getElementById('audio-control').innerHTML = '&#9208;';
           for(var ci = 0; ci < big.audio.textTracks[0].cues.length; ci++)
             if ((big.audio.textTracks[0].cues[ci].startTime <= big.audio.currentTime)
               && (big.audio.textTracks[0].cues[ci].endTime > big.audio.currentTime)
@@ -44,7 +44,7 @@ window.onload = function() {
             }
         }
         else
-          document.getElementById('audio-control').src = 'data:image/svg+xml;charset=utf-8,%3Csvg%20width%3D%221792%22%20height%3D%221792%22%20viewBox%3D%220%200%201792%201792%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cpath%20d%3D%22M896%20128q209%200%20385.5%20103t279.5%20279.5%20103%20385.5-103%20385.5-279.5%20279.5-385.5%20103-385.5-103-279.5-279.5-103-385.5%20103-385.5%20279.5-279.5%20385.5-103zm384%20823q32-18%2032-55t-32-55l-544-320q-31-19-64-1-32%2019-32%2056v640q0%2037%2032%2056%2016%208%2032%208%2017%200%2032-9z%22%20fill%3D%22%23ccc%22%2F%3E%3C%2Fsvg%3E';
+          document.getElementById('audio-control').innerHTML = '&#9654;';
 
       }, 200);
       break;
