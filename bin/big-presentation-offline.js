acca98e9eace0eb325d0219859ba73701befd34a
Tom MacWright <tom@macwright.org>
2018-05-09 11:56:27 -0700

    Add tests for offline

--- bin/big-presentation-offline.js
+++ bin/big-presentation-offline.js
@@ -1,19 +1,29 @@
 #!/usr/bin/env node
-var Inliner = require('inliner');
-var http = require('http');
-var ecstatic = require('ecstatic');
-var getPort = require('get-port');
-var fs = require('fs');
+var Inliner = require("inliner");
+var http = require("http");
+var ecstatic = require("ecstatic");
+var getPort = require("get-port");
+var fs = require("fs");
 
-getPort().then(function(port) {
-  var server = http
-    .createServer(ecstatic({ root: process.cwd() }))
-    .listen(port, function() {
-      new Inliner('http://localhost:' + port, function(error, html) {
-        // compressed and inlined HTML page
-        fs.writeFileSync('./index.offline.html', html);
-        console.log('Done! Wrote index.offline.html with inlined resources');
-        server.close();
-      });
+function offline() {
+  return new Promise(function(resolve) {
+    getPort().then(function(port) {
+      var server = http
+        .createServer(ecstatic({ root: process.cwd() }))
+        .listen(port, function() {
+          new Inliner("http://localhost:" + port, function(error, html) {
+            // compressed and inlined HTML page
+            fs.writeFileSync("./index.offline.html", html);
+            console.log(
+              "Done! Wrote index.offline.html with inlined resources"
+            );
+            server.close();
+            resolve();
+          });
+        });
     });
-});
+  });
+}
+
+module.exports = offline;
+if (require.main === module) offline(process);
