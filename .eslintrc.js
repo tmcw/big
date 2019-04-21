0aecc47661d926b7ed3ef9925d208426e560250a
Tom MacWright <tom@macwright.org>
2019-04-21 15:43:06 -0700

    refactor: Remove IE11 support
    Removing IE11 support allows us to use many better JavaScript patterns:
    
    - Array.from() instead of a function to do the same
    - { once: true } in event listener callbacks
    - Arrow functions, let, const

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    "no-console": 0,
+    "prefer-const": 2,
+    "no-var": 2
+  }
+};
