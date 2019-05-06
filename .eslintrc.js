77bb58acb954e116713ce0736cc56e62f79ae829
Tom MacWright <tom@macwright.org>
2019-05-05 20:27:11 -0700

    refactor: Remove yarn.lock

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,16 +11,5 @@ module.exports = {
     "no-console": 0,
     "prefer-const": 2,
     "no-var": 2
-  },
-  overrides: [
-    {
-      files: ["bin/big.js"],
-      env: {
-        node: true
-      },
-      rules: {
-        "no-unused-expressions": "off"
-      }
-    }
-  ]
+  }
 };
